5159d63371fd051f98d6b6039de6033cb83120f9
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-11-14 23:10:36 +0600

    changes some code

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,3 +1,51 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// module.exports = function (UPLOADS_FOLDER) {
+//   const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, UPLOADS_FOLDER); // Use the provided destination folder
+//     },
+//     filename: (req, file, cb) => {
+//       const fileExt = path.extname(file.originalname);
+//       const filename =
+//         file.originalname
+//           .replace(fileExt, "")
+//           .toLocaleLowerCase()
+//           .split(" ")
+//           .join("-") +
+//         "-" +
+//         Date.now();
+
+//       cb(null, filename + fileExt);
+//     },
+//   });
+
+//   const upload = multer({
+//     storage: storage,
+//     limits: {
+//       fileSize: 200000000000000000000000000, 
+//     },
+//     fileFilter: (req, file, cb) => {
+//       if (
+//         file.mimetype == "image/jpg" ||
+//         file.mimetype == "image/png" ||
+//         file.mimetype == "image/jpeg" ||
+//         file.mimetype == "image/heic" ||
+//         file.mimetype == "image/heif"
+//       ) {
+//         cb(null, true);
+
+//       } else {
+//         cb(new Error("Only jpg, png, jpeg format allowed!"));
+//       }
+//     },
+//   });
+
+//   return upload; // Return the configured multer upload middleware
+// };
+
+
 const multer = require("multer");
 const path = require("path");
 
@@ -11,7 +59,7 @@ module.exports = function (UPLOADS_FOLDER) {
       const filename =
         file.originalname
           .replace(fileExt, "")
-          .toLocaleLowerCase()
+          .toLowerCase()
           .split(" ")
           .join("-") +
         "-" +
@@ -21,26 +69,35 @@ module.exports = function (UPLOADS_FOLDER) {
     },
   });
 
+  const allowedTypes = [
+    "application/pdf",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/heic",
+    "image/heif",
+    "audio",
+    "video"
+  ];
+
   const upload = multer({
     storage: storage,
     limits: {
-      fileSize: 200000000000000000000000000, // 20MB
+      fileSize: 15 * 1024 * 1024 * 1024, 
     },
     fileFilter: (req, file, cb) => {
-      if (
-        file.mimetype == "image/jpg" ||
-        file.mimetype == "image/png" ||
-        file.mimetype == "image/jpeg" ||
-        file.mimetype == "image/heic" ||
-        file.mimetype == "image/heif"
-      ) {
-        cb(null, true);
+      const fileType = file.mimetype.split("/")[0]; 
 
+      if (allowedTypes.includes(file.mimetype) || allowedTypes.includes(fileType)) {
+        cb(null, true);
       } else {
-        cb(new Error("Only jpg, png, jpeg format allowed!"));
+        cb(new Error("Invalid file type"));
       }
     },
   });
 
   return upload; // Return the configured multer upload middleware
 };
+
